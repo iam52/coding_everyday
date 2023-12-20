166806243eecd1235655c0e9e496d4f428b8b3ec
newbie_all_the_time <131854898+iam52@users.noreply.github.com>
2023-12-20 21:33:03 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,16 @@
+def solution(A, B):
+    A, B = list(A), list(B)
+
+    for i in range(len(A)):
+        if A == B:
+            return i
+        temp = A.pop()
+        A.insert(0, temp)
+    return -1
+'''
+A와 B를 리스트로 만들고 A의 길이만큼 for 루프를 돌면서,
+A와 B가 같으면 i를 바로 반환하고(i=0),
+A에서 제일 뒤에 문자를 pop해서 A의 0번 인덱스에 삽입하면서 순환,
+A가 B와 같아지면 i를 반환,
+루프를 다 돌아도 맞지 않으면 -1 반환
+'''
