acac383a5fbc64cfe94c88d5465fe7de105e1711
newbie_all_the_time <131854898+iam52@users.noreply.github.com>
2023-12-21 17:24:08 +0900

    Update 특이한 정렬.py

--- 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.py
@@ -3,3 +3,9 @@ def solution(numlist, n):
     answer = []
     answer = sorted(numlist, key=lambda x: (abs(x-n), -x))
     return answer
+
+'''
+abs(x-n)는 n으로부터의 가까운 순서대로 정렬하고 -x는 정렬된 수 중 거리가 같은 값이 나오면 더 큰 값을 먼저 정렬하도록 정의,
+(sorted(key=는 첫번째 기준으로 값이 같을 때 두 번째 조건 적용)
+그 후, 오름 차순으로 결과값 반환
+'''
