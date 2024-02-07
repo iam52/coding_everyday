a95abb6c4eac0e56159dcbce5f6d28453887cc8a
Iksoo <131854898+iam52@users.noreply.github.com>
2024-02-07 11:37:23 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    nums = "0123456789"
+    for i in my_string:
+        if i in nums:
+            answer.append(int(i))
+            answer = sorted(answer)
+    return answer
