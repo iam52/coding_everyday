045f76a7a39add8f9cbd8a83288eb2b5c85daa78
Iksoo <131854898+iam52@users.noreply.github.com>
2024-02-13 11:02:43 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        if i ** 2 == n:
+            answer = (i+1) ** 2
+            break
+        else:
+            answer = -1
+    return answer
