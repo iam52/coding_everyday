3b53b831064caec60cb76f6daac01b277616e35b
Iksoo <131854898+iam52@users.noreply.github.com>
2024-03-28 09:29:55 +0900

    [level 1] Title: 최소직사각형, Time: 4.34 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,9 @@
+def solution(sizes):
+    width = []
+    height = []
+
+    for size in sizes:
+        width.append(max(size))
+        height.append(min(size))
+
+    return max(width) * max(height)
