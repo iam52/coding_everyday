aa45e8a46ba391f816133ce95fc7bf0926b9b0d2
Iksoo <131854898+iam52@users.noreply.github.com>
2024-04-18 09:39:33 +0900

    [level 1] Title: 소수 찾기, Time: 120.46 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,7 +1,17 @@
 def solution(n):
-    num = set(range(2, n+1))
+    # 에라토스테네스의 체
+    prime = [True] * (n+1) # True 값이 들어간 배열 n만큼 생성
+    p = 2 # 제일 작은 소수 2부터 시작
     
-    for i in range(2, n+1):
-        if i in num:
-            num -= set(range(2*i, n+1, i))
-    return len(num)
+    while (p*p <= n): # p의 배수가 n보다 작을 동안,
+        if prime[p] == True: # 만들어 놓은 배열의 p번째가 True이면,
+            for i in range(p*p, n+1, p): # 이미 제외한 배수를 찾을 필요 없이 p*p부터 배수 탐색 후 False
+                prime[i] = False
+        p += 1 # n번째까지 반복
+        
+    prime_nums = []
+    for p in range(2, n+1)    :
+        if prime[p]:
+            prime_nums.append(p) # 가장 작은 소수 2부터 정해진 수까지 만들어 놓은 배열 prime에서 True인 값을 저장
+    
+    return len(prime_nums)
