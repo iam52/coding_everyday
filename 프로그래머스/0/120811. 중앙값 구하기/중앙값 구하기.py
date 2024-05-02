ea5650ff3dca4ce53aa40f94354963c30c80df88
Iksoo <131854898+iam52@users.noreply.github.com>
2024-05-03 08:43:54 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,7 +1,3 @@
 def solution(array):
-    answer = 0
-    array = sorted(array)
-    if len(array) % 2 == 1:
-        idx = len(array) // 2
-    answer = array[idx]
-    return answer
+    array.sort()
+    return array[len(array) // 2]
