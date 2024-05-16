40dda948d71005e9006974bc9b38592e7df5405d
Iksoo <131854898+iam52@users.noreply.github.com>
2024-05-16 09:46:45 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,7 +1,9 @@
 def solution(num_list):
-    answer = -1
+    answer = 0
     for i in range(len(num_list)):
         if num_list[i] < 0:
             answer = i
             break
+        else:
+            answer = -1
     return answer
