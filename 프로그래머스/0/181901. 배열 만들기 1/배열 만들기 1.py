e267be20522f2c4b9a6e3311b3141b68f7681de3
Iksoo <131854898+iam52@users.noreply.github.com>
2024-05-16 09:51:07 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,7 +1,6 @@
 def solution(n, k):
     answer = []
-    for i in range(1, n+1):
-        if i % k == 0:
-            answer.append(i)
-            nu_ans = sorted(answer)
+    for i in range(k, n+1, k):
+        answer.append(i)
+        answer.sort()
     return answer
