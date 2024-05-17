e9e75eada74e355016584717266a06b150aa5209
Iksoo <131854898+iam52@users.noreply.github.com>
2024-05-17 09:48:39 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,10 +1,11 @@
 def solution(a, b):
     answer = 0
-    concat1 = str(a) + str(b)
-    concat2 = str(b) + str(a)
-    if int(concat1) > int(concat2):
-        answer = int(concat1)
+    plus1 = int(str(a) + str(b))
+    plus2 = int(str(b) + str(a))
+    
+    if plus1 > plus2 or plus1 == plus2:
+        answer = plus1
     else:
-        answer = int(concat2)
+        answer = plus2
     
     return answer
