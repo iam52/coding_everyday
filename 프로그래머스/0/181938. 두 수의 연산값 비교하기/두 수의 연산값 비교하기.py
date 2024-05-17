399579f81188bebf0e240fcf02ae19ec2b11268f
Iksoo <131854898+iam52@users.noreply.github.com>
2024-05-17 09:47:22 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,10 +1,11 @@
 def solution(a, b):
     answer = 0
-    conc = str(a) + str(b)
-    if int(conc) > 2 * a * b:
-        answer = int(conc)
-    elif int(conc) < 2 * a * b:
-        answer = 2 * a * b
-    elif int(conc) == 2 * a * b:
-        answer = int(conc)
+    plus = int(str(a) + str(b))
+    multi = 2 * a * b
+    
+    if plus > multi or plus == multi:
+        answer = plus
+    else:
+        answer = multi
+        
     return answer
