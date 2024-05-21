6aa6eeb7aa8490f0254692629d713f27f430e73f
Iksoo <131854898+iam52@users.noreply.github.com>
2024-05-21 10:45:20 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,5 +1,5 @@
 def solution(numbers):
     answer = 0
-    nu_num = sorted(numbers)
-    answer = nu_num[-1] * nu_num[-2]
+    numbers.sort()
+    answer = numbers[-1] * numbers[-2]
     return answer
