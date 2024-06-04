843bc3549e37f67bc8f13d0bed9e9fafd54ddffc
Iksoo <131854898+iam52@users.noreply.github.com>
2024-06-04 10:08:34 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,8 +1,25 @@
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = my_string.trim().split("\\s+");
-        return answer;
+        List<String> words = new ArrayList<>();
+        StringBuilder word = new StringBuilder();
+        
+        for (int i=0; i<my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (c != ' ') {
+                word.append(c);
+            } else if (word.length() > 0) {
+                words.add(word.toString());
+                word.setLength(0);
+            }
+        }
+        
+        if (word.length() > 0) {
+            words.add(word.toString());
+        }
+        return words.toArray(new String[0]);
     }
 }
