1324027c7f26275ad9983839635c5ebb6a9e2b56
Iksoo <131854898+iam52@users.noreply.github.com>
2024-06-13 11:12:29 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 196 ms, Memory: 17576 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        for (int i=0; i<str.length(); i++) {
-            char ch = str.charAt(i);
-            if ('A' <= ch && ch <= 'Z')
+        String word = sc.next();
+        
+        for (int i=0; i<word.length(); i++) {
+            char ch = word.charAt(i);
+            
+            if ('A' <= ch && ch <= 'Z') {
                 System.out.print((char)('a' + ch - 'A'));
-            else 
+            } else{
                 System.out.print((char)('A' + ch - 'a'));
+            }
         }
     }
 }
