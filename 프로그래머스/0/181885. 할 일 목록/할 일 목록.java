7c9860787ad6b573e8e68701f521ee43bf543274
Iksoo <131854898+iam52@users.noreply.github.com>
2024-06-20 09:15:41 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> unfinishedTasks = new ArrayList<>();
+        
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                unfinishedTasks.add(todo_list[i]);
+            }
+        }
+        return unfinishedTasks.toArray(new String[0]);
+    }
+}
