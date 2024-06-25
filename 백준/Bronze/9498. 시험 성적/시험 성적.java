5ebb86b92ac4ed5beb0f8f1238aeb338b67c3d83
Iksoo <131854898+iam52@users.noreply.github.com>
2024-06-25 10:18:04 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        long score = Long.parseLong(st.nextToken());
+
+        char grade = ' ';
+        if (score >= 90 && score <= 100) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println(grade);
+    }
+}
