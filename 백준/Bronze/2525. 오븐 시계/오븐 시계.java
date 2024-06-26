1891e26793491b0cdcd049351e72dfbaf9bbbf7e
Iksoo <131854898+iam52@users.noreply.github.com>
2024-06-26 10:30:40 +0900

    [Bronze III] Title: 오븐 시계, Time: 208 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        B += C;
+        A += B / 60;
+        A %= 24;
+        B %= 60;
+        
+        System.out.println(A + " " + B);
+    }
+}
