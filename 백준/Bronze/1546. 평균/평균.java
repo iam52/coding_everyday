a2def7bec3cf035eb279f10e49a3f66c1c1f7aae
Iksoo <131854898+iam52@users.noreply.github.com>
2024-07-02 10:37:03 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,21 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] score = new int[N];
-        for (int i=0; i<N; i++) {
-            score[i] = sc.nextInt();
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
         int max = 0;
         int sum = 0;
-        for (int i=0; i<N; i++) {
-            if (score[i] > max) {
-                max = score[i];
+        
+        for (int i = 0; i < N; i++) {
+            int scores = Integer.parseInt(st.nextToken());
+            
+            if (scores > max) {
+                max = scores;
             }
-            sum += score[i];
+            sum += scores;
         }
         System.out.println(sum * 100.0 / max / N);
     }
