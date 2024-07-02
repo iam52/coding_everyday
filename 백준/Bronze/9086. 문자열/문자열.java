926f3cb98f98a7931eae1329a2d56cf990b90587
Iksoo <131854898+iam52@users.noreply.github.com>
2024-07-02 17:31:07 +0900

    [Bronze V] Title: 문자열, Time: 204 ms, Memory: 17492 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for (int i = 0; i < N; i++) {
+            String a = sc.next();
+            
+            System.out.print(a.charAt(0));
+            System.out.println(a.charAt(a.length() - 1));
+        }
+    }
+}
