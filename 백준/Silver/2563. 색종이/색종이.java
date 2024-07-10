9ad940f49c4af1f98514326149297658c7a4fd09
Iksoo <131854898+iam52@users.noreply.github.com>
2024-07-10 10:16:20 +0900

    [Silver V] Title: 색종이, Time: 180 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[][] paper = new int[100][100];
+        
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    paper[k][j] = 1;
+                }
+            }
+        }
+        
+        int area = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j] == 1) {
+                    area++;
+                }
+            }
+        }
+        
+        System.out.println(area);
+    }
+}
