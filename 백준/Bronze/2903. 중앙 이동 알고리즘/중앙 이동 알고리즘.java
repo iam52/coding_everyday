2b3e3d011772afa02d8a7e6f7237dcd0aadea269
Iksoo <131854898+iam52@users.noreply.github.com>
2024-07-12 10:16:34 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -2,21 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        int square = 0;
-        int gap = 3;
-
-        for (int i = 0; i <= N; i++) {
-            square = (int) Math.pow(4, i);
-        }
-
-        for (int i = 1; i <= N; i++) {
-            int dot = ((int) Math.pow(2, i));
-            gap += dot;
-        }
-
-        System.out.println(square + gap);
+        int n = sc.nextInt();
+        
+        int pointsOnOneSide = (int) Math.pow(2, n) + 1;
+        int totalPoints = pointsOnOneSide * pointsOnOneSide;
+        
+        System.out.println(totalPoints);
     }
 }
