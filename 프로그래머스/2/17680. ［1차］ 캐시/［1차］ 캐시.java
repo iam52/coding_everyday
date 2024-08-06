92d391f0d0880117700967b66ff064c5df4425e9
Iksoo <131854898+iam52@users.noreply.github.com>
2024-08-06 22:48:45 +0900

    [level 2] Title: [1차] 캐시, Time: 50.46 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        int cacheHit = 1;
+        int cacheMiss = 5;
+        
+        ArrayList<String> cache = new ArrayList<>();
+        
+        for (String city : cities) {
+            city = city.toLowerCase();
+            
+            if (cache.contains(city)) {
+                answer += cacheHit;
+                cache.remove(city);
+                cache.add(city);
+            } else {
+                answer += cacheMiss;
+                if (cache.size() >= cacheSize && cacheSize > 0) {
+                    cache.remove(0);
+                }
+                if (cacheSize > 0) {
+                    cache.add(city);
+                }
+            }
+        }
+        return answer;
+    }
+}
