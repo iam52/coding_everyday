8423b33ea8d618d67eb5acc80d6409ae206f8ece
Iksoo <131854898+iam52@users.noreply.github.com>
2024-08-07 23:05:35 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        
+        StringBuilder result = new StringBuilder();
+        
+        String vowels = "aeiou";
+        
+        for (char c : my_string.toCharArray()) {
+            if (vowels.indexOf(c) == -1) {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+}
