c5de371aede359ad6fc1de0f868f699d93335ee5
Iksoo <131854898+iam52@users.noreply.github.com>
2024-08-22 22:28:43 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.27 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -8,14 +8,16 @@ class Solution {
         long[] tile = new long[n+1];
         tile[0] = 1;
         tile[2] = 3;
-        
+
+        long sum = tile[0];
+
         for (int i = 4; i <= n; i += 2) {
-            tile[i] = tile[i-2] * 3;
-            for (int j = i-4; j >= 0; j -= 2) {
-                tile[i] += tile[j] * 2;
-            }
+            tile[i] = tile[i-2] * 3 + sum * 2;
             tile[i] %= mod;
+            sum += tile[i-2];
+            sum %= mod;
         }
+
         return (int) tile[n];
     }
 }
