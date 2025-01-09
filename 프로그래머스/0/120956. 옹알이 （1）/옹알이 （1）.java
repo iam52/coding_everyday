17d342bdaae559f9bf556f7cdbe61252ea40b470
ois <131854898+iam52@users.noreply.github.com>
2025-01-09 16:21:43 +0900

    [level 0] Title: 옹알이 (1), Time: 0.99 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,13 +3,12 @@ class Solution {
         int answer = 0;
         
         for (String word : babbling) {
-            word = word.replace("aya", " ");
-            word = word.replace("ye", " ");
-            word = word.replace("woo", " ");
-            word = word.replace("ma", " ");
-            
-            word = word.replace(" ", "");
-            
+            word = word
+                .replace("aya", " ")
+                .replace("ye", " ")
+                .replace("woo", " ")
+                .replace("ma", "")
+                .replace(" ", "");
             if (word.isEmpty()) answer++;
         }
         return answer;
