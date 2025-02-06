5bfee9ba30436c15c2b5f93df3ebcba6fb65d7ca
ois <131854898+iam52@users.noreply.github.com>
2025-02-06 17:38:38 +0900

    [level 2] Title: [1차] 캐시, Time: 73.85 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#qna) 
 
 ### 성능 요약
 
-메모리: 121 MB, 시간: 51.79 ms
+메모리: 110 MB, 시간: 73.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 08일 21:06:08
+2025년 02월 06일 17:38:37
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,30 +1,64 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
-        int cacheHit = 1;
-        int cacheMiss = 5;
         
-        ArrayList<String> cache = new ArrayList<>();
+        int time = 0;
+        String[] inCache = new String[cacheSize];
         
-        for (String city : cities) {
-            city = city.toLowerCase();
+        for (int i = 0; i < cities.length; i++) {
             
-            if (cache.contains(city)) {
-                answer += cacheHit;
-                cache.remove(city);
-                cache.add(city);
+            cities[i] = cities[i].toUpperCase();
+            
+            if (cacheSize == 0) {
+                time = cities.length * 5;
+            } else if (!isContains(inCache, cities[i])) {
+                time += 5;
+                updateCache(inCache, cities[i]);
             } else {
-                answer += cacheMiss;
-                if (cacheSize > 0 && cache.size() >= cacheSize) {
-                    cache.remove(0);
-                }
-                if (cacheSize > 0) {
-                    cache.add(city);
-                }
+                time++;
+                updateHitCache(inCache, cities[i]);
             }
         }
-        return answer;
+        
+        return time;
+    }
+    
+    private static boolean isContains(String[] caches, String value) {
+        value.toLowerCase();
+        for (String cache : caches) {
+            if (value.equals(cache)) {
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private static void updateCache(String[] currentCache, String city) {
+        for (int i = currentCache.length - 1; i > 0; i--) {
+            currentCache[i] = currentCache[i - 1];
+        }
+        
+        currentCache[0] = city;
+    }
+    
+    private static void updateHitCache(String[] currentCache, String value) {
+        int hitIndex = -1;
+        
+        for (int i = 0; i < currentCache.length; i++) {
+            if (value.equals(currentCache[i])) {
+                hitIndex = i;
+                break;
+            }
+        }
+        
+        String temp = currentCache[hitIndex];
+        for (int i = hitIndex; i > 0; i--) {
+            currentCache[i] = currentCache[i - 1];
+        }
+        
+        currentCache[0] = temp;
     }
 }
