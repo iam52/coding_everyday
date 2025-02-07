35bfed3fa2e18430f07749026bd2a1722f9154fe
ois <131854898+iam52@users.noreply.github.com>
2025-02-07 11:01:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.08 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,20 +1,16 @@
 class Solution
 {
-    public int solution(int n, int a, int b) {
-        
+    public int solution(int n, int a, int b)
+    {
         int answer = 0;
         
-        for (int i = 0; i < n; i++) {
-            int group1 = (int) ((a - 1) / Math.pow(2, i));
-            int group2 = (int) ((b - 1) / Math.pow(2, i));
-            
-            if (group1 == group2) {
+        for (int i = 1; i <= n; i++) {
+            if ((int) ((a-1) / Math.pow(2, i)) == (int) ((b-1) / Math.pow(2, i))) {
+                answer = i;
                 break;
             }
-            
-            answer++;
         }
-        
+
         return answer;
     }
 }
