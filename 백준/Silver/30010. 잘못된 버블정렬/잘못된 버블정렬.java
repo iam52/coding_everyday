6beca8b4d435904250796a3911a2e2449da1f9c9
ois <131854898+iam52@users.noreply.github.com>
2025-03-04 11:02:09 +0900

    [Silver V] Title: 잘못된 버블정렬, Time: 200 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Silver/30010. 잘못된 버블정렬/잘못된 버블정렬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for (int i = N; i >= 1; i--) {
+            System.out.println(i);
+            
+            if (i > 1) System.out.print(" ");
+        }
+        
+    }
+}
