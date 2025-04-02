3f91692b5ab18dfa4826d22815c20c977c329cf7
ois <131854898+iam52@users.noreply.github.com>
2025-04-02 11:26:13 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 46072 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,42 +1,31 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        int[] nArr = new int[n];
-        for (int i = 0; i < nArr.length; i++) {
-            nArr[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        Set<String> nSet = new HashSet<>(n);
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            nSet.add(st.nextToken());
         }
-
-        Arrays.sort(nArr);
-
-        int m = sc.nextInt();
-        int[] mArr = new int[m];
-        for (int i = 0; i < mArr.length; i++) {
-            mArr[i] = sc.nextInt();
+        
+        int m = Integer.parseInt(br.readLine());
+        
+        st = new StringTokenizer(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (st.hasMoreTokens()) {
+            String num = st.nextToken();
+            sb.append(nSet.contains(num) ? 1 : 0).append("\n");
         }
-
-        for (int target : mArr) {
-            int left = 0;
-            int right = nArr.length - 1;
-            int found = 0;
-
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-
-                if (nArr[mid] == target) {
-                    found = 1;
-                    break;
-                } else if (nArr[mid] < target) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-            System.out.println(found);
-
-        }
+        
+        System.out.print(sb);
     }
 }
