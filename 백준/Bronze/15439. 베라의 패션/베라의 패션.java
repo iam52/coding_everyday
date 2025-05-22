3e1104c55677d19f7f9284c65a1d7e6eb891f910
ois <131854898+iam52@users.noreply.github.com>
2025-05-22 10:24:22 +0900

    [Bronze IV] Title: 베라의 패션, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                } else {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
