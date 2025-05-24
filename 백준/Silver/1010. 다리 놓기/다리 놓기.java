2ce0dc4f9cfdf3bc0d19ef374cc95d22a1a37d2a
ois <131854898+iam52@users.noreply.github.com>
2025-05-24 19:27:10 +0900

    [Silver V] Title: 다리 놓기, Time: 216 ms, Memory: 18264 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            System.out.println(combination(m, n));
+        }
+    }
+
+    public static BigInteger combination(int m, int n) {
+        return factorial(m)
+                .divide(factorial(n).multiply(factorial(m - n)));
+    }
+
+    public static BigInteger factorial(int num) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= num; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+}
