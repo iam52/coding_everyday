b7a1781614adf25db82688296ae296f16c8c1cc5
ois <131854898+iam52@users.noreply.github.com>
2025-06-04 11:45:33 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                dp[i] = 0;
+            } else if (i == 1) {
+                dp[i] = 1;
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+
+        System.out.println(dp[dp.length - 1]);
+    }
+}
